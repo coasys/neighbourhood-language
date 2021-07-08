38d7561c7eefb2f0d8f5c8015ad60cee9b295bcd
jdeepee <joshuadparkin@gmail.com>
2021-07-08 12:36:41 +0100

    add missing dna registration

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import type { Address, Agent, Language, LanguageContext, ExpressionUI, Interaction } from "@perspect3vism/ad4m";
+import type { Address, Agent, Language, LanguageContext, ExpressionUI, Interaction, HolochainLanguageDelegate } from "@perspect3vism/ad4m";
 import Adapter from "./adapter";
+import { DNA, DNA_NICK } from "./dna";
 
 function interactions(a: Agent, expression: Address): Interaction[] {
   return [];
@@ -17,7 +18,9 @@ export class UI implements ExpressionUI {
 
 export const name = "neighbourhood-store";
 
-export default function create(context: LanguageContext): Language {
+export default async function create(context: LanguageContext): Language {
+  const Holochain = context.Holochain as HolochainLanguageDelegate;
+  await Holochain.registerDNAs([{ file: DNA, nick: DNA_NICK }]);
   const expressionAdapter = new Adapter(context);
   const expressionUI = new UI();
 
